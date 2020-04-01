c2bbfc86ae589c552d98ff147f93e9def620be60
skrrrm <a@macbook-windows>
2020-04-01 19:16:37 +0200

    Add project files.

---
+++ KEAOpgave4.8/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KEAOpgave4._8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(64, 77);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(250, 704);
+            this.listBoxResult.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a number for the amount of fibonacci\r\nnumbers you want in the big box:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(350, 150);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(185, 31);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(350, 208);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(185, 44);
+            this.buttonCalc.TabIndex = 3;
+            this.buttonCalc.Text = "Start";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(350, 288);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 851);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxResult);
+            this.Name = "Form1";
+            this.Text = "Fibonacci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ KEAOpgave4.8/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave4._8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            listBoxResult.Items.Clear(); // Clearing listbox
+
+            // Variables
+            int sum, x = 0, y = 1, i = 0, count = 0;
+
+            // try-catch for wrong input
+            try
+            {
+                i = Convert.ToInt32(textBoxInput.Text); // Converting text to int
+            }
+            catch
+            {
+                MessageBox.Show("Numbers only!", "ERROR!", MessageBoxButtons.OK); // error
+            }
+
+            // adding zero and 1 to the first two items of the list (first two fibonacci numbers)
+            listBoxResult.Items.Add(x);
+            listBoxResult.Items.Add(y);
+
+            // + 2 cause were adding the first two fibonacci numbers to the list outside of the loop.
+            // This way the input number of [i] corresponds to the number of items in the list.
+            // Maybe not the best solution?
+            while (count + 2 < i)
+            {
+                sum = x + y; // 0+1=1,  1+1=2,  1+2=3,  3+2=5,  5+3=8 and so on
+                listBoxResult.Items.Add(sum); // adding sum to list while the loop runs
+
+                // See sum comment right above
+                x = y;  
+                y = sum; 
+
+                count++; // so we can use user input for how many fibonacci numbers we want to show
+            }
+        }
+    }
+}
